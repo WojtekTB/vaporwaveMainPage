bfd0000c9e6bce2c74ba8b88dd8aa78ba93d288f
WojtekTB <34536619+WojtekTB@users.noreply.github.com>
2019-04-19 23:51:06 -0700

    made buildings work

--- buildings.js
+++ buildings.js
@@ -1,53 +1,103 @@
 var colorScheme = {
-  fbColor: [121,142,176],
-  bbColor: null
+  fbColor: [121, 142, 176],
+  bbColor: [158,136,158]
 }
 
 class FrontBuilding{
   constructor(x, y){
     let maxHeight = 20;
-    let minHeight = 10;
+    let minHeight = 13;
     let maxWidth = 6;
-    let minWidth = 2;
-    let scale = 10;
+    let minWidth = 3;
+    this.scale = 10;
     this.x = x;
     this.y = y;
     this.colorScheme = colorScheme;
-    this.buildingHeight = Math.floor(random(minHeight, maxHeight)) * scale;
-    this.buildingWidth = Math.floor(random(minWidth, maxWidth)) * scale;
-    this.windowPadding = this.buildingWidth/15;
-    this.windowWandH = scale;
+    this.buildingHeight = Math.floor(random(minHeight, maxHeight)) * this.scale;
+    this.buildingWidth = Math.floor(random(minWidth, maxWidth)) * this.scale;
+
+    this.windowPadding = (this.scale/5);
+    this.windowWandH = this.scale - (this.windowPadding * 2);
     this.windows = [];
-    for(let y = 0; y < (this.buildingWidth - this.windowPadding)/scale; y++){
-      for(let x = 0; x < (this.buildingHeight - this.windowPadding)/scale; x++){
-        this.windows.push([(((this.windowPadding * 2) * x) + (this.windowWandH * x) + this.windowPadding), (((this.windowPadding * 2) * y) + (this.windowWandH * y) + this.windowPadding)]);//[x, y]
+    for(let y = 0; y < (Math.floor((this.buildingHeight - (2 * this.scale))/this.scale)); y++){
+      for(let x = 0; x < Math.floor((this.buildingWidth-this.scale)/this.scale); x++){
+        this.windows.push([(((this.windowPadding * 2) * y) + (this.windowWandH * y) + this.windowPadding),
+          (this.windowPadding * 3.5) + (this.windowWandH * x) + (2 * this.windowPadding * x), Math.floor(random(0, 150))]);
       }
     }
-
+    // console.log(this.windows[0][2]);
+    // console.log({x: this.x, y: this.y, w: this.buildingWidth, h: this.buildingHeight, WandH: this.windowWandH});
   }
   show(){
-    stroke(0);
-    fill(this.colorScheme.fbColor);
-    rect(this.x, this.y, this.buildingWidth, this.buildingHeight);
     noStroke();
-    fill(255, 255, 0, 90);
-    // console.log(this.windows.length);
+    fill(this.colorScheme.fbColor);
+    rect(this.x, this.y - this.buildingHeight, this.buildingWidth, this.buildingHeight);
     for(let i = 0; i < this.windows.length; i++){
-      rect(this.windows[i][1] + this.x, this.windows[i][0] + this.y, this.windowWandH, this.windowWandH);
-      // console.log(this.windows[i][0], this.windows[i][1], this.windowWandH);
+      if(this.windows[i][2] === 0){
+        fill(179,196,161);
+      }
+      else if(this.windows[i][2] === 1){
+        fill(153,206,251);
+      }
+      // else if(this.windows[i][2] === 3){
+      //   fill(this.colorScheme.fbColor);
+      // }
+      else{
+        fill(122, 155, 189);
+      }
+      rect(this.windows[i][1] + this.x, this.windows[i][0] + this.y - this.buildingHeight + this.windowPadding, this.windowWandH, this.windowWandH);
     }
   }
   update(){}
 }
 
 class BackBuilding{
-  constructor(colorScheme){
-    this.MaxHeight = 40;
-    this.MinHeight = 10;
-    this.MaxWidth = 6;
-    this.MinWidth = 3;
+  constructor(x, y){
+    let maxHeight = 40;
+    let minHeight = 23;
+    let maxWidth = 6;
+    let minWidth = 3;
     this.scale = 10;
+    this.x = x;
+    this.y = y;
+    this.colorScheme = colorScheme;
+    this.buildingHeight = Math.floor(random(minHeight, maxHeight)) * this.scale;
+    this.buildingWidth = Math.floor(random(minWidth, maxWidth)) * this.scale;
+
+    this.windowPadding = (this.scale/5);
+    this.windowWandH = this.scale - (this.windowPadding * 2);
+    this.windows = [];
+    for(let y = 0; y < (Math.floor((this.buildingHeight - (2 * this.scale))/this.scale)); y++){
+      for(let x = 0; x < Math.floor((this.buildingWidth-this.scale)/this.scale); x++){
+        this.windows.push([(((this.windowPadding * 2) * y) + (this.windowWandH * y) + this.windowPadding),
+          (this.windowPadding * 3.5) + (this.windowWandH * x) + (2 * this.windowPadding * x)]);
+      }
+    }
+    // console.log(this.windows[0][2]);
+    // console.log({x: this.x, y: this.y, w: this.buildingWidth, h: this.buildingHeight, WandH: this.windowWandH});
   }
-  show(){}
+  show(){
+    noStroke();
+    fill(this.colorScheme.bbColor);
+    rect(this.x, this.y - this.buildingHeight, this.buildingWidth, this.buildingHeight);
+    for(let i = 0; i < this.windows.length; i++){
+      fill(165,143,165);
+      // console.log(this.windows[i][2]);
+      rect(this.windows[i][1] + this.x, this.windows[i][0] + this.y - this.buildingHeight + this.windowPadding, this.windowWandH, this.windowWandH);
+    }
+  }
   update(){}
+}
+
+let startColors = {
+  c1: 217,148,184,
+  c1: 246,246,246,
+  c1: 217,148,184,
+}
+
+class Stars{
+  constructor(){
+
+  }
+
 }
--- sketch.js
+++ sketch.js
@@ -1,11 +1,54 @@
 var screenX = window.innerWidth, screenY = window.innerHeight;
-var building;
+var frontBuildings = [];
+var backBuildings = [];
 
 function setup(){
+  let totalYoffset = (screenY)/10;
+  let bby = screenY - totalYoffset;
+  let fby = screenY - totalYoffset;
   createCanvas(window.innerWidth, window.innerHeight);
-  building = new FrontBuilding(200, 300);
+  backBuildings.push(new BackBuilding(0, bby));
+  for(let i = 0; i < (screenX / ((6+3)/2)); i++){
+    backBuildings.push(new BackBuilding(backBuildings[i].x + backBuildings[i].buildingWidth + random(-5, 5), bby));
+  }
+  frontBuildings.push(new FrontBuilding(0, fby));
+  for(let i = 0; i < (screenX / ((6+3)/2)); i++){
+    frontBuildings.push(new FrontBuilding(frontBuildings[i].x + frontBuildings[i].buildingWidth + random(-5, 5), fby));
+  }
+
+
+  setGradient(0, 0, screenX, screenY, color(189,130,143), color(143,121,170), 1);
+
+  for(let i = 0; i < frontBuildings.length; i++){
+    backBuildings[i].show();
+  }
+  for(let i = 0; i < frontBuildings.length; i++){
+    frontBuildings[i].show();
+  }
 }
 function draw(){
-  background(0);
-  building.show();
+  //189,130,143
+  //143,121,145
+}
+
+function setGradient(x, y, w, h, c1, c2, axis) {
+  noFill();
+
+  if (axis === 1) {
+    // Top to bottom gradient
+    for (let i = y; i <= y + h; i++) {
+      let inter = map(i, y, y + h, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(x, i, x + w, i);
+    }
+  } else if (axis === 2) {
+    // Left to right gradient
+    for (let i = x; i <= x + w; i++) {
+      let inter = map(i, x, x + w, 0, 1);
+      let c = lerpColor(c1, c2, inter);
+      stroke(c);
+      line(i, y, i, y + h);
+    }
+  }
 }
